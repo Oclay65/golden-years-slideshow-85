e791112a1976ac98a372006a2843ba322971426a
Oclay65 <dtolan46@gmail.com>
2025-05-29 23:57:55 -0400

    Update slideshow to remove captions and add new images

--- src/components/Slideshow.tsx
+++ src/components/Slideshow.tsx
@@ -6,123 +6,123 @@ const Slideshow = () => {
   const images = [
     {
       src: '/lovable-uploads/90119110-a910-4e62-b732-42b5b2e89a0f.png',
-      caption: 'Gathering together, sharing stories and laughter'
+      caption: ''
     },
     {
       src: '/lovable-uploads/2fa7fc04-aecf-4f3a-94d7-440a919895e0.png',
-      caption: 'Warm smiles and cherished friendships'
+      caption: ''
     },
     {
       src: '/lovable-uploads/8c6cae49-036f-4744-a95d-bce2c2fd8c39.png',
-      caption: 'The music and memories that bring us together'
+      caption: ''
     },
     {
       src: '/lovable-uploads/70994c8f-a78b-42db-9917-b7a7ca03fdd3.png',
-      caption: 'A wonderful evening filled with joy and celebration'
+      caption: ''
     },
     {
       src: '/lovable-uploads/7b89d453-797d-45a9-93d0-b22661536fe4.png',
-      caption: 'Dancing and celebrating our class reunion'
+      caption: ''
     },
     {
       src: '/lovable-uploads/fd76bf61-0aea-45a0-b639-cf4038b8fc9c.png',
-      caption: 'Archer HS Class of 1985 - Together again with endless smiles'
+      caption: ''
     },
     {
       src: '/lovable-uploads/9830edc8-5761-4a0a-9cde-579b9ca1f146.png',
-      caption: 'Class unity in purple and gold - Archer High School pride'
+      caption: ''
     },
     {
       src: '/lovable-uploads/958898b9-d557-44a2-89ef-c546da502bef.png',
-      caption: 'Friends celebrating under the tent - Class of \'85 spirit'
+      caption: ''
     },
     {
       src: '/lovable-uploads/cdea4522-80bf-4fb1-b9ca-e1102106f298.png',
-      caption: 'Playground fun - reliving our youthful energy together'
+      caption: ''
     },
     {
       src: '/lovable-uploads/4da4106d-a0c6-41f4-b5de-bcf5efdfc408.png',
-      caption: 'Nature backdrop - Class of 1985 in perfect harmony'
+      caption: ''
     },
     {
       src: '/lovable-uploads/8dab02bf-5e15-4c41-b2b9-61cd423c0057.png',
-      caption: 'Honoring our memories - a beautiful memorial display'
+      caption: ''
     },
     {
       src: '/lovable-uploads/a727bd97-8dee-4f19-9e39-b7c9206d04bc.png',
-      caption: 'Casual moments and genuine connections'
+      caption: ''
     },
     {
       src: '/lovable-uploads/247d46d7-d48f-4466-b281-a8c428d426f7.png',
-      caption: 'Unexpected reunions - the joy of reconnecting with classmates'
+      caption: ''
     },
     {
       src: '/lovable-uploads/4f9ba17f-9c15-44ce-a42a-3442a622ae4e.png',
-      caption: 'Class of 1985 celebration cakes - marking our reunion'
+      caption: ''
     },
     {
       src: '/lovable-uploads/3e757bcf-15ed-42ee-adef-6d1ecc5a6616.png',
-      caption: 'Happy 55th Birthday celebration - still celebrating together'
+      caption: ''
     },
     {
       src: '/lovable-uploads/846fd208-4120-4382-b896-12d0478fa611.png',
-      caption: 'Elegant moments - dressed up and feeling great'
+      caption: ''
     },
     {
       src: '/lovable-uploads/1815a0d3-430e-40c8-9fe0-3630719c1ab4.png',
-      caption: 'Dancing with style - keeping the party alive'
+      caption: ''
     },
     {
       src: '/lovable-uploads/116c5d82-195b-499e-ab3d-98c1a4902638.png',
-      caption: 'Seated together - sharing memories and catching up'
+      caption: ''
     },
     {
       src: '/lovable-uploads/4811af3e-9231-42e9-85f3-00437e27db53.png',
-      caption: 'Victory celebration - class spirit never dies'
+      caption: ''
     },
     {
       src: '/lovable-uploads/1a6d9e9a-3380-49d3-99c7-7d9bdd474b58.png',
-      caption: 'DJ booth fun - keeping the music and energy flowing'
+      caption: ''
     },
     {
       src: '/lovable-uploads/8c7de5f7-ef6c-4d47-9ece-5bb13aa50d8f.png',
-      caption: 'Sweet embrace - 55th birthday love and friendship'
+      caption: ''
     },
     {
       src: '/lovable-uploads/a7912338-536c-4290-8588-c5ef8e9607c9.png',
-      caption: 'Purple and gold pride - Archer High School Class of 1985'
+      caption: ''
     },
     {
       src: '/lovable-uploads/a474238b-4e1c-4986-b7b6-fbd1ae141835.png',
-      caption: 'Dance floor energy - the Class of 1985 still knows how to party'
+      caption: ''
     },
     {
       src: '/lovable-uploads/_60A4709.png',
-      caption: 'Celebrating lifelong bonds and friendships'
+      caption: ''
     },
     {
       src: '/lovable-uploads/_60A4708.png',
-      caption: 'Joy and laughter - memories that last forever'
+      caption: ''
     },
     {
-      src: '/lovable-uploads/IMG_0082 (2).jpg',
-      caption: 'Class of 1985 reunion - forever connected'
+      src: '/lovable-uploads/IMG_0088 (1).jpg',
+      caption: ''
     },
     {
-      src: '/lovable-uploads/IMG_0071 (2).jpg',
-      caption: 'Celebrating under the tent - summer reunion fun'
+      src: '/lovable-uploads/IMG_0090 (1).jpg',
+      caption: ''
     },
     {
-      src: '/lovable-uploads/IMG_0085 (1).jpg',
-      caption: 'Grilling and chilling with classmates'
+      src: '/lovable-uploads/IMG_0092 (1).jpg',
+      caption: ''
     },
     {
-      src: '/lovable-uploads/IMG_0086 (1).jpg',
-      caption: 'Relaxing and reminiscing with old friends'
+      src: '/lovable-uploads/IMG_0104 (1).jpg',
+      caption: ''
     },
     {
-      src: '/lovable-uploads/FullSizeR (3).jpg',
-      caption: 'Happy 55th birthday celebration in style'
+      src: '/lovable-uploads/IMG_0110 (1).jpg',
+      caption: ''
     }
   ];
 
@@ -206,7 +206,7 @@ const Slideshow = () => {
             >
               <img
                 src={image.src}
-                alt={image.caption}
+                alt=""
                 className="w-full h-full object-contain bg-black"
                 style={{ objectPosition: 'center center' }}
               />
@@ -255,12 +255,6 @@ const Slideshow = () => {
           >
             {isFullscreen ? <Minimize className="h-6 w-6" /> : <Maximize className="h-6 w-6" />}
           </Button>
-        </div>
-
-        <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/80 to-transparent p-6">
-          <p className="text-yellow-200 text-xl font-medium text-center animate-fade-in">
-            {images[currentIndex].caption}
-          </p>
         </div>
       </div>
 
